5aed2d7a4c4000a811e7bcf52918c0c22d8d70d7
hubert kiedrowski <hubert.kiedr@gmail.com>
2022-10-22 16:48:00 +0200

    server eingerichtet

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-test
+const express= require("express");
+const app = express();
+app.use(express.urlencoded({extended:true}))
+
+app.engine("ejs", require("ejs").__express);
+app.set("view engine", "ejs");
+
+const DATABASE ="login.db";
+const db =require("better-sqlite3")(DATABASE);
+
+//server starten
+app.listen(4242, function(){
+    console.log("http://localhost:4242/anmeldedaten");
+});
+
+app.get("/startseite",function(req,res){
+        res.sendFile(__dirname + "/views/startseite.html")
+});
