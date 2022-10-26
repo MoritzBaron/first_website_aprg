f0f5bf7dee3bed2c5d1be2c7053a8eb41bf0cfaf
JonathanM2548 <Jonathan.Magdanz@haw-hamburg.de>
2022-10-26 12:37:45 +0200

    Datenbank hinzugefügt, nodemon dependency

---
+++ benutzerdaten.sql
@@ -0,0 +1,10 @@
+/*Datenbank initialisieren mit .read*/
+
+/*Tabelle mit benutzer und passwort*/
+
+CREATE TABLE benutzer(
+    benutzername TEXT NOT NULL,
+    passwort TEXT NOT NULL
+);
+
+/* Probe Datensätze*/
