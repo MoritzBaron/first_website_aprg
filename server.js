f26adc3b9e958a7a0665a02d233820f839ceb7fc
Hubert Kiedrowski <73240170+hubertkiedrowski@users.noreply.github.com>
2022-11-03 22:20:38 +0100

    login redesign

--- server.js
+++ server.js
@@ -1,96 +1,97 @@
 //Express initialisieren
-const express= require("express");
+const express = require("express");
 const app = express();
-app.use(express.urlencoded({extended:true}));
-
-
+app.use(express.urlencoded({ extended: true }));
 
 //Ejs initialisiern
 app.engine("ejs", require("ejs").__express);
 app.set("view engine", "ejs");
 
 //freigabe der ordner
-app.use(express.static(__dirname +'/views'));
-app.use(express.static(__dirname +'/pictures'));
-app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/pictures"));
+app.use(express.static(__dirname + "/public"));
 
 //initialisierung Datenbank
 const DATABASE = "benutzer.db";
-const db =  require("better-sqlite3")(DATABASE);
+const db = require("better-sqlite3")(DATABASE);
 
 // init express fileupload
-const fileUpload = require('express-fileupload');
+const fileUpload = require("express-fileupload");
 app.use(fileUpload());
 //cookies-parser init
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 //functions
-function anmeldung(benutzername, passwort){
+function anmeldung(benutzername, passwort) {
     const benutzer = db.prepare("SELECT * FROM benutzer").all();
-    for (daten of benutzer){
-        if(daten.benutzername == benutzername && daten.passwort == passwort ){
+    for (daten of benutzer) {
+        if (daten.benutzername == benutzername && daten.passwort == passwort) {
             return true;
         }
-
     }
-    return false; 
-};
-
-
+    return false;
+}
 
 //server starten
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("http://localhost:3000/startseite");
 });
 
-
 //get Requests
-app.get("/startseite",function(req,res){
-    res.sendFile(__dirname + "/views/startseite.html"); 
+app.get("/startseite", function (req, res) {
+    res.sendFile(__dirname + "/views/startseite.html");
 });
 
-app.get("/features",function(req,res){
+app.get("/features", function (req, res) {
     res.sendFile(__dirname + "/views/features.html");
 });
 
-app.get("/aboutUs",function(req,res){
+app.get("/aboutUs", function (req, res) {
     res.sendFile(__dirname + "/views/aboutUs.html");
 });
 /*app.get("/login",function(req,res){
     res.sendFile(__dirname + "/views/loginformular.html")
 });*/
-app.get("/login",function(req,res){
-    res.render("login")
+app.get("/login", function (req, res) {
+    res.render("login");
 });
 
-app.get("/registrierung", function(req,res){
-    res.render("registrierung")
+app.get("/registrierung", function (req, res) {
+    res.render("registrierung");
 });
 
 //Post Requests
-app.post("/login", function(req,res){
-});
+app.post("/login", function (req, res) {});
 
-app.post("/registrierung", function(req,res){
-});
+app.post("/registrierung", function (req, res) {});
 
-app.post("/neuerBenutzer", function(req, res){
+app.post("/neuerBenutzer", function (req, res) {
     const param_name = req.body.name;
     const param_email = req.body.email;
-    const param_passwort = req.body.passwort
+    const param_passwort = req.body.passwort;
 
-    const info =db.prepare("INSERT INTO benutzer (email, benutzername, passwort) VALUES(?,?,?)").run(param_email, param_name, param_passwort);
+    const info = db
+        .prepare(
+            "INSERT INTO benutzer (email, benutzername, passwort) VALUES(?,?,?)"
+        )
+        .run(param_email, param_name, param_passwort);
     console.log(info);
     res.redirect("/login");
-})
+});
 
 // Anmeldung
-app.post("/startseite", function(req, res){
+app.post("/startseite", function (req, res) {
     const benutzername = req.body.benutzername;
     const passwort = req.body.passwort;
 
-    if (anmeldung(benutzername, passwort)){
+    if (anmeldung(benutzername, passwort)) {
         res.redirect("/startseite");
     }
+});
+
+
+app.get("/kalender", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
 });
