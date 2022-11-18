e43f3d4151acfa68c35d436488862f473e659942
Moritz Baron <115716375+GrumpyGrape@users.noreply.github.com>
2022-11-18 12:15:12 +0100

    speicherung

--- views/kalenderMo.js
+++ views/kalenderMo.js
@@ -15,6 +15,7 @@ export class Kalender{
         this.weekOffSet = 0;
         this.mode = MODE.VIEW;
         this.events = {};
+        this.slotHeight = 30;
     }
     setup(){
         this.setupTimes();
@@ -22,6 +23,7 @@ export class Kalender{
         this.calculateCurrentWeek();
         this.showWeek();
         this.setupControls();
+        this.loadEvents();
     }
 
     setupTimes(){
@@ -190,6 +192,7 @@ export class Kalender{
         this.weekStart = addDays(this.weekStart, 7 * number);
         this.weekEnd = addDays(this.weekEnd, 7 * number);
         this.showWeek();
+        this.loadEvents();
     }
 
     showCurrentDay(){
@@ -203,6 +206,11 @@ export class Kalender{
     }
 
     saveEvents(){
-        this.events
+        ("events", JSON.stringify(this.events));
+    }
+
+    loadEvents(){
+        //in setup + changeWeek
+        
     }
 }
